636bc71e244c79bc6befe3aac549fbb7c19dcab2
ninevra <ninevra@gmail.com>
2020-04-17 00:25:10 -0700

    Test proxy 'getOwnPropertyDescriptor' invariants

--- test/proxy-invariants.js
+++ test/proxy-invariants.js
@@ -3,6 +3,12 @@ const scoped = require('../index');
 const expect = require('chai').use(require('sinon-chai')).expect;
 const sinon = require('sinon');
 
+// These tests attempt to perform edge-case operations on the filesView Proxy,
+// to trigger invariant violations if possible. Several interact with systems
+// that metalsmith-scoped doesn't ever touch, just in case. The actual
+// assertions aren't particularly necessary, as violating an invariant will
+// throw a TypeError and fail the test anyway.
+
 describe('proxy invariants', function () {
   // Extract the filesView proxy from a scoped plugin invocation
   function getProxy(patterns, files) {
@@ -59,7 +65,7 @@ describe('proxy invariants', function () {
 
   describe('ownKeys', function () {
     it('should return an array of strings and symbols', async function () {
-      const obj = { swallow: 'coconut', larch: 'birch'};
+      const obj = { swallow: 'coconut', larch: 'the'};
       obj[Symbol('bridge')] = 'doom';
       const proxy = await getProxy(['*a*'], obj);
       for (let key of Reflect.ownKeys(proxy)) {
@@ -86,12 +92,41 @@ describe('proxy invariants', function () {
   });
 
   describe('getOwnPropertyDescriptor', function () {
-    it('should return a valid property descriptor or undefined');
+    it('should return a valid property descriptor or undefined', async function () {
+      const obj = {a: 1};
+      const proxy = await getProxy(['**/*', obj]);
+      expect(Object.defineProperty({}, Object.getOwnPropertyDescriptor(proxy, 'a'))).to.not.throw();
+      expect(Object.getOwnPropertyDescriptor(proxy, 'b')).to.be.undefined;
+    });
 
-    it('should report existence of non-configurable own properties');
+    it('should report existence of non-configurable own properties', async function () {
+      const obj = {};
+      Object.defineProperty(obj, 'swallow', {
+        configurable: false,
+        value: 'coconut'
+      });
+      const proxy = await getProxy(['larch'], obj);
+      expect(Object.getOwnPropertyDescriptor(proxy, 'swallow')).to.exist;
+    });
 
-    it('should truthfully report existence or non-existence of own properties of non-extensible objects');
+    it('should truthfully report existence or non-existence of own properties of non-extensible objects', async function () {
+      const obj = {a: 1, b: 2, c: 3, d: 4};
+      Object.preventExtensions(obj);
+      const proxy = await getProxy(['a*', 'b*'], obj);
+      expect(Object.getOwnPropertyDescriptor(proxy, 'c')).to.exist;
+      expect(Object.getOwnPropertyDescriptor(proxy, 'a')).to.exist;
+      expect(Object.getOwnPropertyDescriptor(proxy, 'e')).to.be.undefined;
+    });
 
-    it('should only report non-configurability when it\'s really there')
+    it('should only report non-configurability when it\'s really there', async function () {
+      const obj = {a: 1};
+      Object.defineProperty(obj, 'b', {
+        configurable: false,
+        value: 2
+      });
+      const proxy = await getProxy(['*'], obj);
+      expect(Object.getOwnPropertyDescriptor(proxy, 'a').configurable).to.be.true;
+      expect(Object.getOwnPropertyDescriptor(proxy, 'b').configurable).to.be.false;
+    });
   });
 });
