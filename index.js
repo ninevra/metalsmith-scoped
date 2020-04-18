3d0c2c06160024eee90ed85bbfd7f51a1ab6a807
ninevra <ninevra@gmail.com>
2020-04-18 15:58:07 -0700

    Implement write-blocking

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ function scoped(plugin, patterns, multimatchOptions) {
   return function scopedPlugin(files, metalsmith, callback) {
     function shouldBlock(property) {
       return ((typeof property) !== 'symbol'
-              && files.hasOwnProperty(property)
+              && files.hasOwnProperty(property) // TODO: maybe enumerability would be a better check here?
               && multimatch(property, patterns, multimatchOptions).length === 0)
+    }
+    function shouldWriteBlock(property) {
+      return ((typeof property) !== 'symbol'
+              && multimatch(property, patterns, multimatchOptions).length === 0);
     }
     const filesView = new Proxy(files, {
       get: function (target, property) {
@@ -34,6 +38,28 @@ function scoped(plugin, patterns, multimatchOptions) {
           return undefined;
         } else {
           return Reflect.getOwnPropertyDescriptor(target, property);
+        }
+      },
+      defineProperty: function (target, property, descriptor) {
+        if (shouldWriteBlock(property)) {
+          throw new Error(`${plugin.name} tried to define ${property}, out of scope ${patterns}`);
+        } else {
+          return Reflect.defineProperty(target, property, descriptor);
+        }
+      },
+      set: function (target, property, value) {
+        if (shouldWriteBlock(property)) {
+          throw new Error(`${plugin.name} tried to set ${property}, out of scope ${patterns}`);
+        } else {
+          return Reflect.set(target, property, value); // TODO: add receivers here and elsewhere
+        }
+      },
+      deleteProperty: function (target, property) {
+        // TODO: should this return 'true' rather than throw?
+        if (shouldWriteBlock(property)) {
+          throw new Error(`${plugin.name} tried to delete ${property}, out of scope ${patterns}`);
+        } else {
+          return Reflect.deleteProperty(target, property);
         }
       }
     });
